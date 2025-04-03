57a8f64874a4fbb897279e0e90f2908ba8d3f8b3
Lpi <thelpi.v3@gmail.com>
2025-04-03 23:10:36 +0200

    migration

--- ExplorerFM/Datas/Player.cs
+++ ExplorerFM/Datas/Player.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using ExplorerFM.FieldsAttributes;
 using ExplorerFM.Providers;
 
